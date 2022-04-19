02336b5000af7467d636091b4eef11b69376ad60
琦玉 <wangzhenjie1221@gmail.com>
2022-04-19 13:54:13 +0800

    509. 斐波那契数 dp

--- arithmetic/src/main/java/leetcode_202204/QiYu20220418.java
+++ arithmetic/src/main/java/leetcode_202204/QiYu20220418.java
@@ -172,4 +172,53 @@ public class QiYu20220418 {
             System.out.println(maxSatisfaction(new int[]{4, 3, 2, 0}));
         }
     }
+
+    /**
+     * 509. 斐波那契数
+     * 斐波那契数 （通常用 F(n) 表示）形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+     *
+     * F(0) = 0，F(1) = 1
+     * F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+     * 给定 n ，请计算 F(n) 。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：n = 2
+     * 输出：1
+     * 解释：F(2) = F(1) + F(0) = 1 + 0 = 1
+     * 示例 2：
+     *
+     * 输入：n = 3
+     * 输出：2
+     * 解释：F(3) = F(2) + F(1) = 1 + 1 = 2
+     * 示例 3：
+     *
+     * 输入：n = 4
+     * 输出：3
+     * 解释：F(4) = F(3) + F(2) = 2 + 1 = 3
+     *
+     *
+     * 提示：
+     *
+     * 0 <= n <= 30
+     */
+    static class Solution2 {
+        public static int fib(int n) {
+            int[] fib = new int[n+1];
+            if (n < 1) {
+                return fib[n];
+            }
+            fib[1] = 1;
+            for (int i = 2; i <= n; i++) {
+                fib[i] = fib[i-1] + fib[i-2];
+            }
+            return fib[n];
+        }
+
+        public static void main(String[] args) {
+            System.out.println(fib(30));
+        }
+    }
 }
