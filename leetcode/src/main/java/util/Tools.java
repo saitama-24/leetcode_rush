8807c624a36286e867663bb66004cca7d8d3dc16
琦玉 <wangzhenjie1221@gmail.com>
2022-04-28 10:57:05 +0800

    techniques.two_pointers.sort_array_by_parity-ac1

--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("three-steps-problem-lcci"));
+        System.out.println(conventSpecChar("sort-array-by-parity"));
     }
 }
