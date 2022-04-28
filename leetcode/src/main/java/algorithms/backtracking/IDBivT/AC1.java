140480468e186ac6feffdf606493d359586dd0e1
琦玉 <wangzhenjie1221@gmail.com>
2022-04-28 17:58:23 +0800

    //todo 剑指 Offer II 085. 生成匹配的括号 回溯算法

---
+++ leetcode/src/main/java/algorithms/backtracking/IDBivT/AC1.java
@@ -0,0 +1,41 @@
+package algorithms.backtracking.IDBivT;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName: AC1
+ * @Description: 剑指 Offer II 085. 生成匹配的括号
+ * @Author: 琦玉
+ * @Date: 2022年04月28日 17:51:39
+ */
+public class AC1 {
+
+    /**
+     * 剑指 Offer II 085. 生成匹配的括号
+     * 正整数 n 代表生成括号的对数，请设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：n = 3
+     * 输出：["((()))","(()())","(())()","()(())","()()()"]
+     * 示例 2：
+     *
+     * 输入：n = 1
+     * 输出：["()"]
+     *
+     *
+     * 提示：
+     *
+     * 1 <= n <= 8
+     *
+     */
+    static class Solution {
+        public List<String> generateParenthesis(int n) {
+            // todo 回溯
+            return new ArrayList<>();
+        }
+    }
+}
