aff33536ae9c95160a9fa08f1431c89051e4b2e6
琦玉 <wangzhenjie1221@gmail.com>
2022-05-05 16:47:34 +0800

    122. 买卖股票的最佳时机 II dp

--- leetcode/src/main/java/algorithms/dynamic_programming/best_time_to_buy_and_sell_stock_ii/AC2.java
+++ leetcode/src/main/java/algorithms/dynamic_programming/best_time_to_buy_and_sell_stock_ii/AC2.java
@@ -6,7 +6,7 @@ package algorithms.dynamic_programming.best_time_to_buy_and_sell_stock_ii;
  * @Author: 琦玉
  * @Date: 2022年05月05日 16:19:59
  */
-public class AC1 {
+public class AC2 {
 
     /**
      * 122. 买卖股票的最佳时机 II
@@ -44,21 +44,22 @@ public class AC1 {
      * 0 <= prices[i] <= 10^4
      */
     static class Solution {
+        // 优化空间复杂度至O(1)
         public static int maxProfit(int[] prices) {
             int len = prices.length;
-            int[][] dp = new int[len][2];
-            dp[0][0] = 0;
-            dp[0][1] = -prices[0];
-
+            int dp0 = 0;
+            int dp1 = -prices[0];
             for (int i = 1; i < len; i++) {
-                dp[i][0] = Math.max(dp[i - 1][0], prices[i] + dp[i - 1][1]);
-                dp[i][1] = Math.max(dp[i - 1][0] - prices[i], dp[i - 1][1]);
+                int newDp0 = Math.max(dp0, prices[i] + dp1);
+                int newDp1 = Math.max(dp0 - prices[i], dp1);
+                dp0 = newDp0;
+                dp1 = newDp1;
             }
-            return dp[len - 1][0];
+            return dp0;
         }
 
         public static void main(String[] args) {
-            System.out.println(maxProfit(new int[]{7, 6, 4, 3, 1}));
+            System.out.println(maxProfit(new int[]{1,2,3,4,5}));
         }
     }
 }
