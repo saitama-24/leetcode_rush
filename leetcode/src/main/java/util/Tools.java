6ffc21d47ddd1243443c6541ea4896b3cffd72ca
琦玉 <wangzhenjie1221@gmail.com>
2022-05-06 18:03:26 +0800

    121. 买卖股票的最佳时机 dp 123. 买卖股票的最佳时机 III todo

--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("number-of-recent-calls"));
+        System.out.println(conventSpecChar("best-time-to-buy-and-sell-stock-iii"));
     }
 }
