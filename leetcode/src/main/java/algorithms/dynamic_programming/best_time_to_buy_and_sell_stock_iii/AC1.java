8333424bed69be88667178f3a0f4019a8b042436
琦玉 <wangzhenjie1221@gmail.com>
2022-05-06 18:20:44 +0800

    123. 买卖股票的最佳时机 III dp

--- leetcode/src/main/java/algorithms/dynamic_programming/best_time_to_buy_and_sell_stock_iii/AC1.java
+++ leetcode/src/main/java/algorithms/dynamic_programming/best_time_to_buy_and_sell_stock_iii/AC1.java
@@ -44,12 +44,25 @@ public class AC1 {
      *
      * 提示：
      *
-     * 1 <= prices.length <= 105
-     * 0 <= prices[i] <= 105
+     * 1 <= prices.length <= 10^5
+     * 0 <= prices[i] <= 10^5
      */
     static class Solution {
         public static int maxProfit(int[] prices) {
+            int n = prices.length;
+            int buy1 = -prices[0], sell1 = 0;
+            int buy2 = -prices[0], sell2 = 0;
+            for (int i = 1; i < n; ++i) {
+                buy1 = Math.max(buy1, -prices[i]);
+                sell1 = Math.max(sell1, buy1 + prices[i]);
+                buy2 = Math.max(buy2, sell1 - prices[i]);
+                sell2 = Math.max(sell2, buy2 + prices[i]);
+            }
+            return sell2;
+        }
 
+        public static void main(String[] args) {
+            System.out.println(maxProfit(new int[]{3,3,5,0,0,3,1,4}));
         }
     }
 }
