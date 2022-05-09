5d8c791fc41bd82637e7eaf698a8d964c510ae41
琦玉 <wangzhenjie1221@gmail.com>
2022-05-09 17:37:26 +0800

    64. 最小路径和 → AC1

--- leetcode/src/main/java/algorithms/dynamic_programming/minimum_path_sum/AC1.java
+++ leetcode/src/main/java/algorithms/dynamic_programming/minimum_path_sum/AC1.java
@@ -36,29 +36,30 @@ public class AC1 {
      * 0 <= grid[i][j] <= 100
      */
     static class Solution {
-        // todo
         public static int minPathSum(int[][] grid) {
             int col = grid.length;
             int row = grid[0].length;
             int[][] dp = new int[col][row];
             dp[0][0] = grid[0][0];
             for (int i = 1; i < col; i++) {
+                // 求出 i,0 最短路径
                 dp[i][0] = dp[i-1][0] + grid[i][0];
             }
             for (int j = 1; j < row; j++) {
+                // 求出 0,j 最短路径
                 dp[0][j] = dp[0][j-1] + grid[0][j];
             }
             for (int i = 1; i < col; i++) {
                 for (int j = 1; j < row; j++) {
-                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
                 }
             }
             return dp[col-1][row-1];
         }
 
         public static void main(String[] args) {
-            int[] arr1 = new int[]{1,3,1};
-            int[] arr2 = new int[]{1,5,1};
+            int[] arr1 = new int[]{1,2,3};
+            int[] arr2 = new int[]{4,5,6};
             int[] arr3 = new int[]{4,2,1};
             System.out.println(minPathSum(new int[][]{arr1, arr2, arr3}));
         }
