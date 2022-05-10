0069c43a2062543fd10629e6b60b3fffb3ef8996
琦玉 <wangzhenjie1221@gmail.com>
2022-05-10 18:15:08 +0800

    // todo

---
+++ leetcode/src/main/java/algorithms/greedy/largest_odd_number_in_string/AC1.java
@@ -0,0 +1,50 @@
+package algorithms.greedy.largest_odd_number_in_string;
+
+/**
+ * @ClassName: AC1
+ * @Description: 1903. 字符串中的最大奇数
+ * @Author: 琦玉
+ * @Date: 2022年05月10日 18:11:01
+ */
+public class AC1 {
+
+    /**
+     * 1903. 字符串中的最大奇数
+     * 给你一个字符串 num ，表示一个大整数。请你在字符串 num 的所有 非空子字符串 中找出 值最大的奇数 ，并以字符串形式返回。如果不存在奇数，则返回一个空字符串 "" 。
+     *
+     * 子字符串 是字符串中的一个连续的字符序列。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：num = "52"
+     * 输出："5"
+     * 解释：非空子字符串仅有 "5"、"2" 和 "52" 。"5" 是其中唯一的奇数。
+     * 示例 2：
+     *
+     * 输入：num = "4206"
+     * 输出：""
+     * 解释：在 "4206" 中不存在奇数。
+     * 示例 3：
+     *
+     * 输入：num = "35427"
+     * 输出："35427"
+     * 解释："35427" 本身就是一个奇数。
+     *
+     *
+     * 提示：
+     *
+     * 1 <= num.length <= 10^5
+     * num 仅由数字组成且不含前导零
+     */
+    static class Solution {
+        public static String largestOddNumber(String num) {
+            char[] chars = num.toCharArray();
+            for (int i = chars.length - 1; i >= 0; i--) {
+                // todo
+//                if (chars[i] == )
+            }
+        }
+    }
+}
--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("di-string-match"));
+        System.out.println(conventSpecChar("largest-odd-number-in-string"));
     }
 }
