853778c0e9b369938deda4b6b84771aa07d6864e
琦玉 <wangzhenjie1221@gmail.com>
2022-05-11 17:39:37 +0800

    1827. 最少操作使数组递增 → greedy → AC1

--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("minimize-maximum-pair-sum-in-array"));
+        System.out.println(conventSpecChar("minimum-operations-to-make-the-array-increasing"));
     }
 }
