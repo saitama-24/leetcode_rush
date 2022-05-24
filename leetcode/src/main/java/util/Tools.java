d740a979133d5c241628ba393dbefd286440e45c
琦玉 <wangzhenjie1221@gmail.com>
2022-05-24 18:00:29 +0800

    1688. 比赛中的配对次数 → simulation → ac1

--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("same-tree"));
+        System.out.println(conventSpecChar("count-of-matches-in-tournament"));
     }
 }
