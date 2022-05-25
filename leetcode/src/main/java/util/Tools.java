9c476b5b4ffa1efc76817f6e583fc05fe9612cb3
琦玉 <wangzhenjie1221@gmail.com>
2022-05-25 16:16:40 +0800

    467. 环绕字符串中唯一的子字符串 → dp → ac1

--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("count-of-matches-in-tournament"));
+        System.out.println(conventSpecChar("unique-substrings-in-wraparound-string/solution/-by-cyoushika-045j"));
     }
 }
