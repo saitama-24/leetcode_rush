9f3dd6f98ddb2090d87aed0810957a3221ecb73b
琦玉 <wangzhenjie1221@gmail.com>
2022-05-25 18:02:23 +0800

    518. 零钱兑换 II → dp → ac1

--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("unique-substrings-in-wraparound-string/solution/-by-cyoushika-045j"));
+        System.out.println(conventSpecChar("coin-change-2"));
     }
 }
