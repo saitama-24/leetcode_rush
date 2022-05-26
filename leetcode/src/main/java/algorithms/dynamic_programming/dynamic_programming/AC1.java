c6b9d66980a3cca833a734a81746e354914846bf
琦玉 <wangzhenjie1221@gmail.com>
2022-05-26 15:18:53 +0800

    983. 最低票价 → dp → ac1

---
+++ leetcode/src/main/java/algorithms/dynamic_programming/dynamic_programming/AC1.java
@@ -0,0 +1,90 @@
+package algorithms.dynamic_programming.dynamic_programming;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @ClassName: AC1
+ * @Description: 983. 最低票价
+ * @Author: 琦玉
+ * @Date: 2022年05月26日 11:20:48
+ */
+public class AC1 {
+
+    /**
+     * 983. 最低票价
+     * 在一个火车旅行很受欢迎的国度，你提前一年计划了一些火车旅行。在接下来的一年里，你要旅行的日子将以一个名为 days 的数组给出。每一项是一个从 1 到 365 的整数。
+     *
+     * 火车票有 三种不同的销售方式 ：
+     *
+     * 一张 为期一天 的通行证售价为 costs[0] 美元；
+     * 一张 为期七天 的通行证售价为 costs[1] 美元；
+     * 一张 为期三十天 的通行证售价为 costs[2] 美元。
+     * 通行证允许数天无限制的旅行。 例如，如果我们在第 2 天获得一张 为期 7 天 的通行证，那么我们可以连着旅行 7 天：第 2 天、第 3 天、第 4 天、第 5 天、第 6 天、第 7 天和第 8 天。
+     *
+     * 返回 你想要完成在给定的列表 days 中列出的每一天的旅行所需要的最低消费 。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：days = [1,4,6,7,8,20], costs = [2,7,15]
+     * 输出：11
+     * 解释：
+     * 例如，这里有一种购买通行证的方法，可以让你完成你的旅行计划：
+     * 在第 1 天，你花了 costs[0] = $2 买了一张为期 1 天的通行证，它将在第 1 天生效。
+     * 在第 3 天，你花了 costs[1] = $7 买了一张为期 7 天的通行证，它将在第 3, 4, ..., 9 天生效。
+     * 在第 20 天，你花了 costs[0] = $2 买了一张为期 1 天的通行证，它将在第 20 天生效。
+     * 你总共花了 $11，并完成了你计划的每一天旅行。
+     * 示例 2：
+     *
+     * 输入：days = [1,2,3,4,5,6,7,8,9,10,30,31], costs = [2,7,15]
+     * 输出：17
+     * 解释：
+     * 例如，这里有一种购买通行证的方法，可以让你完成你的旅行计划：
+     * 在第 1 天，你花了 costs[2] = $15 买了一张为期 30 天的通行证，它将在第 1, 2, ..., 30 天生效。
+     * 在第 31 天，你花了 costs[0] = $2 买了一张为期 1 天的通行证，它将在第 31 天生效。
+     * 你总共花了 $17，并完成了你计划的每一天旅行。
+     *
+     *
+     * 提示：
+     *
+     * 1 <= days.length <= 365
+     * 1 <= days[i] <= 365
+     * days 按顺序严格递增
+     * costs.length == 3
+     * 1 <= costs[i] <= 1000
+     */
+    static class Solution {
+        private static Integer[] memo;
+        private static Set<Integer> dayset;
+
+        public static int mincostTickets(int[] days, int[] costs) {
+            memo = new Integer[366];
+            dayset = new HashSet<>();
+            for (int d: days) {
+                dayset.add(d);
+            }
+            return dp(1, costs);
+        }
+
+        public static int dp(int i, int[] costs) {
+            if (i > 365) {
+                return 0;
+            }
+            if (memo[i] != null) {
+                return memo[i];
+            }
+            if (dayset.contains(i)) {
+                memo[i] = Math.min(Math.min(dp(i + 1, costs) + costs[0], dp(i + 7, costs) + costs[1]), dp(i + 30, costs) + costs[2]);
+            } else {
+                memo[i] = dp(i + 1, costs);
+            }
+            return memo[i];
+        }
+
+        public static void main(String[] args) {
+            System.out.println(mincostTickets(new int[]{1,2,3,4,5,6,7,8,9,10,30,31}, new int[]{2,7,15}));
+        }
+    }
+}
--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("coin-change-2"));
+        System.out.println(conventSpecChar("minimum-cost-for-tickets"));
     }
 }
