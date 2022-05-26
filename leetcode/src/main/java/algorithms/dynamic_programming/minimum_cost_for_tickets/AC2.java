a007c6a026366698a2b8a141867de1085bac6f12
琦玉 <wangzhenjie1221@gmail.com>
2022-05-26 16:23:48 +0800

    983. 最低票价 → dp → ac2

--- leetcode/src/main/java/algorithms/dynamic_programming/minimum_cost_for_tickets/AC2.java
+++ leetcode/src/main/java/algorithms/dynamic_programming/minimum_cost_for_tickets/AC2.java
@@ -1,15 +1,12 @@
-package algorithms.dynamic_programming.dynamic_programming;
-
-import java.util.HashSet;
-import java.util.Set;
+package algorithms.dynamic_programming.minimum_cost_for_tickets;
 
 /**
- * @ClassName: AC1
+ * @ClassName: AC2
  * @Description: 983. 最低票价
  * @Author: 琦玉
  * @Date: 2022年05月26日 11:20:48
  */
-public class AC1 {
+public class AC2 {
 
     /**
      * 983. 最低票价
@@ -56,35 +53,25 @@ public class AC1 {
      * 1 <= costs[i] <= 1000
      */
     static class Solution {
-        private static Integer[] memo;
-        private static Set<Integer> dayset;
-
         public static int mincostTickets(int[] days, int[] costs) {
-            memo = new Integer[366];
-            dayset = new HashSet<>();
-            for (int d: days) {
-                dayset.add(d);
+            int len = days.length;
+            int maxDay = days[len - 1];
+            int minDay = days[0];
+            int[] dp = new int[maxDay + 31];
+            for (int day = maxDay, i = days.length - 1; day >= minDay; day--) {
+                if (day == days[i]) {
+                    dp[day] = Math.min(dp[day + 1] + costs[0], dp[day + 7] + costs[1]);
+                    dp[day] = Math.min(dp[day], dp[day + 30] + costs[2]);
+                    i--;
+                } else {
+                    dp[day] = dp[day + 1];
+                }
             }
-            return dp(1, costs);
-        }
-
-        public static int dp(int i, int[] costs) {
-            if (i > 365) {
-                return 0;
-            }
-            if (memo[i] != null) {
-                return memo[i];
-            }
-            if (dayset.contains(i)) {
-                memo[i] = Math.min(Math.min(dp(i + 1, costs) + costs[0], dp(i + 7, costs) + costs[1]), dp(i + 30, costs) + costs[2]);
-            } else {
-                memo[i] = dp(i + 1, costs);
-            }
-            return memo[i];
+            return dp[minDay];
         }
 
         public static void main(String[] args) {
-            System.out.println(mincostTickets(new int[]{1,2,3,4,5,6,7,8,9,10,30,31}, new int[]{2,7,15}));
+            System.out.println(mincostTickets(new int[]{1,4,6,7,8,20}, new int[]{2,7,15}));
         }
     }
 }
