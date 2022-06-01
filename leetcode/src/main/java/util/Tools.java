715d4f3dd0956995cb991e00d3aeadc5766195f3
琦玉 <wangzhenjie1221@gmail.com>
2022-06-01 18:07:44 +0800

    1863. 找出所有子集的异或总和再求和 → backtracking → ac1

--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("partition-array-into-three-parts-with-equal-sum"));
+        System.out.println(conventSpecChar("sum-of-all-subset-xor-totals"));
     }
 }
