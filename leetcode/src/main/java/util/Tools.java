e93e42f208c9cb03e587e41acafd7515fe96713a
琦玉 <wangzhenjie1221@gmail.com>
2022-06-07 16:16:09 +0800

    2089. 找出数组排序后的目标下标 → array → ac1、ac2

--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("koko-eating-bananas"));
+        System.out.println(conventSpecChar("find-target-indices-after-sorting-array"));
     }
 }
