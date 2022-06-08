c66c78f1a6f6fa52cc54c65c35ef96c23ace8bd0
琦玉 <wangzhenjie1221@gmail.com>
2022-06-08 17:55:49 +0800

    769. 最多能完成排序的块 → greedy → ac1

--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("valid-boomerang"));
+        System.out.println(conventSpecChar("max-chunks-to-make-sorted"));
     }
 }
