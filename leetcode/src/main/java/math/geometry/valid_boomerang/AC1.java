229988c4e397d213d1749695570d6aa571527c00
琦玉 <wangzhenjie1221@gmail.com>
2022-06-08 17:09:55 +0800

    1037. 有效的回旋镖 → geometry → ac1

---
+++ leetcode/src/main/java/math/geometry/valid_boomerang/AC1.java
@@ -0,0 +1,59 @@
+package math.geometry.valid_boomerang;
+
+/**
+ * @ClassName: AC1
+ * @Description: 1037. 有效的回旋镖
+ * @Author: 琦玉
+ * @Date: 2022年06月08日 16:36:27
+ */
+public class AC1 {
+
+    /**
+     * 1037. 有效的回旋镖
+     * 给定一个数组 points ，其中 points[i] = [xi, yi] 表示 X-Y 平面上的一个点，如果这些点构成一个 回旋镖 则返回 true 。
+     *
+     * 回旋镖 定义为一组三个点，这些点 各不相同 且 不在一条直线上 。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：points = [[1,1],[2,3],[3,2]]
+     * 输出：true
+     * 示例 2：
+     *
+     * 输入：points = [[1,1],[2,2],[3,3]]
+     * 输出：false
+     *
+     *
+     * 提示：
+     *
+     * points.length == 3
+     * points[i].length == 2
+     * 0 <= xi, yi <= 100
+     */
+    static class Solution {
+        public static boolean isBoomerang(int[][] points) {
+            // 判断不能有相同的数
+            if ((points[2][0] == points[1][0] && points[2][1] == points[1][1]) ||
+                    (points[2][0] == points[0][0] && points[2][1] == points[0][1]) ||
+                    (points[0][0] == points[1][0] && points[1][1] == points[0][1])) {
+                return false;
+            }
+            // 判断三点x和y不能在同一水平线
+            if ((points[2][0] == points[1][0] && points[2][0] == points[0][0]) || (points[2][1] == points[1][1] && points[2][1] == points[0][1])) {
+                return false;
+            }
+            // 判断斜率不同
+            return (double) (points[1][1] - points[0][1]) / (double) (points[1][0] - points[0][0]) != (double) (points[2][1] - points[1][1]) / (double) (points[2][0] - points[1][0]);
+        }
+
+        public static void main(String[] args) {
+            System.out.println(isBoomerang(new int[][]{
+                    {73,31},
+                    {73,19},
+                    {73,45}
+            }));
+        }
+    }
+}
--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("rabbits-in-forest"));
+        System.out.println(conventSpecChar("valid-boomerang"));
     }
 }
