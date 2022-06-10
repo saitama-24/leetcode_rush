12efb3f72e36229f9ab55616fa9f2a753648dc1a
琦玉 <wangzhenjie1221@gmail.com>
2022-06-10 17:21:52 +0800

    134. 加油站 → greedy → ac1

--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("removing-minimum-and-maximum-from-array"));
+        System.out.println(conventSpecChar("gas-station"));
     }
 }
