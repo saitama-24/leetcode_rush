53f72f0b9d7a73dcb5c260a856756d2cf3eb54d6
琦玉 <wangzhenjie1221@gmail.com>
2022-06-23 14:51:57 +0800

    30. 串联所有单词的子串 → sliding_window → ac1

--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("deepest-leaves-sum"));
+        System.out.println(conventSpecChar("substring-with-concatenation-of-all-words"));
     }
 }
