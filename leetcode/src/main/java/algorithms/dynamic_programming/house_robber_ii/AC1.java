f0b8f27b7f2472bea6d590e84f29fc0ed03b588f
琦玉 <wangzhenjie1221@gmail.com>
2022-06-30 11:33:39 +0800

    213. 打家劫舍 II → dp → ac1

--- leetcode/src/main/java/algorithms/dynamic_programming/house_robber_ii/AC1.java
+++ leetcode/src/main/java/algorithms/dynamic_programming/house_robber_ii/AC1.java
@@ -11,51 +11,57 @@ public class AC1 {
     /**
      * 213. 打家劫舍 II
      * 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
-     *
+     * <p>
      * 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * 示例 1：
-     *
+     * <p>
      * 输入：nums = [2,3,2]
      * 输出：3
      * 解释：你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
      * 示例 2：
-     *
+     * <p>
      * 输入：nums = [1,2,3,1]
      * 输出：4
      * 解释：你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
-     *      偷窃到的最高金额 = 1 + 3 = 4 。
+     * 偷窃到的最高金额 = 1 + 3 = 4 。
      * 示例 3：
-     *
+     * <p>
      * 输入：nums = [1,2,3]
      * 输出：3
-     *
-     *
+     * <p>
+     * <p>
      * 提示：
-     *
+     * <p>
      * 1 <= nums.length <= 100
      * 0 <= nums[i] <= 1000
      */
     static class Solution {
-        // todo 待实现
-//        public static int rob(int[] nums) {
-//            int len = nums.length;
-//            if (len == 1) {
-//                return nums[0];
-//            }
-//            int[] dp = new int[len];
-//            dp[0] = nums[0];
-//            dp[1] = Math.max(nums[1], nums[0]);
-//            for (int i = 2; i < dp.length; i++) {
-//                dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
-//            }
-//            return dp[nums.length-1];
-//        }
-//
-//        public static void main(String[] args) {
-//            System.out.println(rob(new int[]{2,1,1,2}));
-//        }
+        public static int rob(int[] nums) {
+            int len = nums.length;
+            if (len == 1) {
+                return nums[0];
+            }
+            return Math.max(rob(nums, 0, len - 1), rob(nums, 1, len));
+        }
+
+        public static int rob(int[] nums, int begin, int end) {
+            if (end - begin == 1) {
+                return nums[end - 1];
+            }
+            int[] dp = new int[nums.length];
+            dp[begin] = nums[begin];
+            dp[begin + 1] = Math.max(nums[begin + 1], dp[begin]);
+            for (int i = begin + 2; i < end; i++) {
+                dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+            }
+            return dp[end - 1];
+        }
+
+        public static void main(String[] args) {
+            System.out.println(rob(new int[]{0,0}));
+        }
     }
 }
