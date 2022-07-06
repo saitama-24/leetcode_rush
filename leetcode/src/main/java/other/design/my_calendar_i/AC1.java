6354ad59ff9edf04002b5a5ec851d49f6aafdf38
琦玉 <wangzhenjie1221@gmail.com>
2022-07-06 10:43:51 +0800

    729. 我的日程安排表 I → design → ac1

---
+++ leetcode/src/main/java/other/design/my_calendar_i/AC1.java
@@ -0,0 +1,71 @@
+package other.design.my_calendar_i;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName: AC1
+ * @Description: 729. 我的日程安排表 I
+ * @Author: 琦玉
+ * @Date: 2022年07月05日 17:02:09
+ */
+public class AC1 {
+
+    /**
+     * 729. 我的日程安排表 I
+     * 实现一个 MyCalendar 类来存放你的日程安排。如果要添加的日程安排不会造成 重复预订 ，则可以存储这个新的日程安排。
+     * <p>
+     * 当两个日程安排有一些时间上的交叉时（例如两个日程安排都在同一时间内），就会产生 重复预订 。
+     * <p>
+     * 日程可以用一对整数 start 和 end 表示，这里的时间是半开区间，即 [start, end), 实数 x 的范围为，  start <= x < end 。
+     * <p>
+     * 实现 MyCalendar 类：
+     * <p>
+     * MyCalendar() 初始化日历对象。
+     * boolean book(int start, int end) 如果可以将日程安排成功添加到日历中而不会导致重复预订，返回 true 。否则，返回 false 并且不要将该日程安排添加到日历中。
+     * <p>
+     * <p>
+     * 示例：
+     * <p>
+     * 输入：
+     * ["MyCalendar", "book", "book", "book"]
+     * [[], [10, 20], [15, 25], [20, 30]]
+     * 输出：
+     * [null, true, false, true]
+     * <p>
+     * 解释：
+     * MyCalendar myCalendar = new MyCalendar();
+     * myCalendar.book(10, 20); // return True
+     * myCalendar.book(15, 25); // return False ，这个日程安排不能添加到日历中，因为时间 15 已经被另一个日程安排预订了。
+     * myCalendar.book(20, 30); // return True ，这个日程安排可以添加到日历中，因为第一个日程安排预订的每个时间都小于 20 ，且不包含时间 20 。
+     * <p>
+     * <p>
+     * 提示：
+     * <p>
+     * 0 <= start < end <= 10^9
+     * 每个测试用例，调用 book 方法的次数最多不超过 1000 次。
+     */
+    static class MyCalendar {
+        List<int[]> booked;
+
+        public MyCalendar() {
+            booked = new ArrayList<>();
+        }
+
+        public boolean book(int start, int end) {
+            for (int[] arr : booked) {
+                int l = arr[0], r = arr[1];
+                if (l < end && start < r) {
+                    return false;
+                }
+            }
+            booked.add(new int[]{start, end});
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyCalendar myCalendar = new MyCalendar();
+        myCalendar.book(3, 20);
+    }
+}
