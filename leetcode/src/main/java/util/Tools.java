31ceb93b442ede1114d09af4be6407a343d54266
琦玉 <wangzhenjie1221@gmail.com>
2022-08-11 17:31:54 +0800

    1417. 重新格式化字符串 → string → ac1

--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("minimum-value-to-get-positive-step-by-step-sum"));
+        System.out.println(conventSpecChar("reformat-the-string"));
     }
 }
