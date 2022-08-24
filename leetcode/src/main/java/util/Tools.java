c7a0bac38561e7779564a42ea3bc072cf76d4183
琦玉 <wangzhenjie1221@gmail.com>
2022-08-24 17:34:06 +0800

    1460. 通过翻转子数组使两个数组相等 → array → ac1

--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("largest-number"));
+        System.out.println(conventSpecChar("make-two-arrays-equal-by-reversing-sub-arrays"));
     }
 }
