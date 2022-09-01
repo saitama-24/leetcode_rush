6a506abf1ab8fa7fc67f18d3112c300e6b0c709b
琦玉 <wangzhenjie1221@gmail.com>
2022-09-01 17:56:58 +0800

    1475. 商品折扣后的最终价格 → stack → ac1

--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("validate-stack-sequences"));
+        System.out.println(conventSpecChar("final-prices-with-a-special-discount-in-a-shop"));
     }
 }
