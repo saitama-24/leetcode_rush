471acadef5cc49d4cf79a77de8f434091c957950
qiyu <wangzhenjie1221@gmail.com>
2022-09-25 16:58:54 +0800

    reset ssh keys.

--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("check-array-formation-through-concatenation"));
+        System.out.println(conventSpecChar("distribute-candies-to-people"));
     }
 }
