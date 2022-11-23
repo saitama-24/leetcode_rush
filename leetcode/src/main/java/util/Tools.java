1bc7cd788c818a695723957ad5ef80806b9ed6f8
qiyu <wangzhenjie1221@gmail.com>
2022-11-23 23:34:57 +0800

    1742. 盒子中小球的最大数量 → hash_table → AC1

--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("container-with-most-water/description"));
+        System.out.println(conventSpecChar("maximum-number-of-balls-in-a-box"));
     }
 }
