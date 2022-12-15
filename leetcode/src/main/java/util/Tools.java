4a37048e1f0d23178aa14d5c4de8808050e68818
qiyu <wangzhenjie1221@gmail.com>
2022-12-15 21:15:07 +0800

    1945. 字符串转化后的各位数字之和 → string → AC1

--- leetcode/src/main/java/util/Tools.java
+++ leetcode/src/main/java/util/Tools.java
@@ -18,6 +18,6 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        System.out.println(conventSpecChar("reverse-linked-list"));
+        System.out.println(conventSpecChar("sum-of-digits-of-string-after-convert"));
     }
 }
